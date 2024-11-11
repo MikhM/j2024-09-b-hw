1ac434519121fcae3e25735bc10e5c43857ebfe2
MikhM <admin@Admins-iMac.local>
2024-11-11 10:23:31 +0300

    the first commit)) with a box print method
    Signed-off-by: MikhM <admin@Admins-iMac.local>

--- src/main/java/ru/otus/java/basic/homeworks/MyApplication.java
+++ src/main/java/ru/otus/java/basic/homeworks/MyApplication.java
@@ -3,5 +3,16 @@ package ru.otus.java.basic.homeworks;
 public class MyApplication {
     public static void main(String[] args) {
         System.out.println("Hello java!");
+        printBox();
+
+    }
+    static  void printBox(){
+
+        System.out.println(";;;;;;;;;;;;;;;;;;;");
+        System.out.println(";-----------------;");
+        System.out.println(";-----------------;");
+        System.out.println(";-----------------;");
+        System.out.println(";;;;;;;;;;;;;;;;;;;");
+
     }
 }
